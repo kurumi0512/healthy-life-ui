1ca5891841bc8c95d035a9fe6f53db63b0d0c269
unknown <foreverlove0512t@gmail.com>
2025-05-22 23:44:00 +0800

    250522 23:43

--- src/pages/SugarLogPage.jsx
+++ src/pages/SugarLogPage.jsx
@@ -47,7 +47,7 @@ function SugarLogPage() {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-8 bg-white rounded-lg shadow-lg">
+    <div className="max-w-4xl mx-auto mt-5 p-8 bg-white rounded-lg shadow-lg">
       <h1 className="text-3xl font-semibold text-center text-gray-800 mb-6">血糖紀錄</h1>
 
       {/* 輸入血糖區塊 */}
--- src/pages/WeightRecordPage.jsx
+++ src/pages/WeightRecordPage.jsx
@@ -43,7 +43,7 @@ const WeightRecordPage = () => {
     };
 
     return (
-        <div className="max-w-4xl mx-auto p-8 bg-white rounded-lg shadow-lg">
+        <div className="max-w-4xl mx-auto mt-5 p-8 bg-white rounded-lg shadow-lg">
             <h1 className="text-3xl font-semibold text-center text-gray-800 mb-6">
                 體重紀錄
             </h1>
